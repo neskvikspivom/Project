6ecca217bef5040a7237b5022f7e98902c40b146
neskvikspivom <ilya.fedotko09@gmail.com>
2022-04-25 20:01:47 +0300

    adding login

--- routes.py
+++ routes.py
@@ -1,9 +1,21 @@
 from app import app
+from flask import render_template, flash, redirect, url_for
+from forms import LoginForm
 
 
 @app.route('/')
 def main_page():
-    return '<h1>Hello!</h1>'
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate():
+        flash(f'Login requested for user {form.username.data}')
+        return redirect(url_for('main_page'))
+    return render_template('login.html', title='Login page', form=form)
+
 
 
 
